bf4b021c24c22b0870675849d76c55a758ed399f
stefanherdy <stefan.herdy@gmail.com>
2023-06-15 12:40:13 +0200

    rename_data

--- find_scale.py
+++ find_scale.py
@@ -1,7 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Script Name: find_scale.py
+Author: Stefan Herdy
+Date: 01.02.2023
+Description: 
+Automatically find and measure a scale in microscopy image
+
+Usage: 
+-  Set your data path and measure the scale of your images
+"""
+
 import cv2
 import numpy as np
 
-def get_scale(img_dir, name, plot = False):
+def get_scale(img_dir, plot = False):
     img = cv2.imread(img_dir)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret,thresh = cv2.threshold(gray,253,255,cv2.THRESH_BINARY)
@@ -28,7 +41,6 @@ def get_scale(img_dir, name, plot = False):
                     cv2.putText(img, 'Scale', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
 
                     print('------------------------')
-                    print(name)
                     print('The length of 1000 Micrometers is ' + str(w) + 'Pixels.')
                     if plot == True:
                         cv2.imshow("Shapes", img)
@@ -37,17 +49,7 @@ def get_scale(img_dir, name, plot = False):
     return w
 
 if __name__ == '__main__':
-    timesteps_int = [0,4,11,18,25,32,39,45,54,60,67,74,81,88,95]
-    timesteps =['0', '04', '11', '18', '25', '32', '39', '45', '54', '60', '67', '74', '81', '88', '95']
-    samplelist = ['06', '37', '39', '40', '44', '45', '46', '47']
-    treatments = ['UN', 'SN', 'UL', 'SL', 'UH', 'SH']
-    # Define the paths to the image and mask directories
-    for _, sample in enumerate(samplelist):
-        for _, treatment in enumerate(treatments):
-            sizes = []
-            for _, day in enumerate(timesteps):
-                name = 'R' + sample + treatment + day + '.jpg'
-                image_dir = './input_data/eval_data/R' + sample + '/' + '/R' + sample + treatment + '/' + name
-                pix = get_scale(image_dir,name)
+    image_dir = './data/Riccia_test.tif'
+    pix = get_scale(image_dir)
 
         
